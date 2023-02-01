e48097466f5ec6eaae3fb6c0fff9c9fbae0b15d9
20FacuSam23 <facundo.samela@gmail.com>
2023-01-31 23:21:24 -0300

    avanzar clase 5 creo

--- Attache/src/app/componentes/cambio/cambio.component.ts
+++ Attache/src/app/componentes/cambio/cambio.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {CargarScriptService} from "src/app/cargar-script.service"
 import { faLinkedin, faYoutube, faFacebook, faInstagram, faGithub, faGoogle} from '@fortawesome/free-brands-svg-icons';
-import { faDownload, faPlus, faNetworkWired, faCircleNodes} from '@fortawesome/free-solid-svg-icons';
-
+import { faDownload, faPlus, faNetworkWired, faCircleNodes, faFingerprint} from '@fortawesome/free-solid-svg-icons';
+import { AttacheService } from 'src/app/servicios/attache.service';
 @Component({
   selector: 'app-cambio',
   templateUrl: './cambio.component.html',
   styleUrls: ['./cambio.component.css']
 })
-export class CambioComponent {
+
+export class CambioComponent implements OnInit {
+  miAttache:any;
   myLinkedin = faLinkedin
   faLinkedin = faLinkedin
   faYoutube = faYoutube
@@ -20,14 +22,18 @@ export class CambioComponent {
   faPLus= faPlus
   faNetworkWired = faNetworkWired
   faCircleNodes = faCircleNodes
+  faFingerprint = faFingerprint
+
+  constructor(private datosAttache:AttacheService,  _CargaScripts:CargarScriptService){ _CargaScripts.Carga(["js/script"]);}
+  ngOnInit():void{
+   this.datosAttache.obtenerDatos().subscribe(data =>{console.log(data);
+    this.miAttache=data;
+  });
+  }
+  }
+
+
   
 
 
-constructor(private _CargaScripts:CargarScriptService)
-{
-  _CargaScripts.Carga(["js/script"]);
-  
-}
-
-}
 
--- Attache/src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ Attache/src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AttacheService } from 'src/app/servicios/attache.service';
 
 @Component({
   selector: 'app-sobre-mi',
   templateUrl: './sobre-mi.component.html',
   styleUrls: ['./sobre-mi.component.css']
 })
-export class SobreMiComponent {
-
-}
+export class SobreMiComponent implements OnInit {
+  miAttache:any;
+  constructor(private datosAttache:AttacheService){}
+  ngOnInit():void{
+   this.datosAttache.obtenerDatos().subscribe(data =>{console.log(data);
+    this.miAttache=data;
+  });
+  }
+  }
